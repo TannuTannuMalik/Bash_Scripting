ddc8ee4b6143d4388bf67516b2cbf530074ffe13
TannuTannuMalik <tannusingh10522@gmail.com>
2025-03-22 21:20:44 +1300

    Updated backup.sh with fixes and improvements

--- task2/backup.sh
+++ task2/backup.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-# Function to display usage instructions
+    # Function to display usage instructions
 usage() {
     echo "Usage: $0 <directory_to_backup>"
     exit 1
 }
 
-# Check if a directory argument is provided
+# Function to check if a directory argument is provided
 if [ -z "$1" ]; then
     echo "No directory provided. Please enter the directory name to backup:"
     read directory_to_backup
@@ -14,30 +14,30 @@ else
     directory_to_backup=$1
 fi
 
-# Check if the provided directory exists
+# To check if the provided directory exists
 if [ ! -d "$directory_to_backup" ]; then
     echo "The directory '$directory_to_backup' does not exist."
     exit 2
 fi
 
-# Get the backup destination directory from user input
-echo "Enter the directory where you want to store the backup (within the Docker container):"
+# To get the backup destination directory from user input
+echo "Enter the directory where you want to store the backup (*within the Docker container):"
 read backup_directory
 
-# Check if the backup directory exists, create if it doesn't
+# 	Its to check if the backup directory exists, create if it doesn't
 if [ ! -d "$backup_directory" ]; then
     echo "Directory does not exist. Creating directory '$backup_directory'."
     mkdir -p "$backup_directory"
 fi
 
-# Define the backup filename with timestamp
+# This funtion define the backup filename with timestamp
 backup_filename="backup_$(basename "$directory_to_backup")_$(date +'%Y%m%d%H%M%S').tar.gz"
 
-# Create a tar.gz archive of the provided directory
+# This will create a tar.gz archive of the provided directory
 echo "Creating backup of '$directory_to_backup'..."
 tar -czf "$backup_directory/$backup_filename" -C "$(dirname "$directory_to_backup")" "$(basename "$directory_to_backup")"
 
-# Check if the backup was successful
+# Function to check if the backup was successful
 if [ $? -eq 0 ]; then
     echo "Backup of '$directory_to_backup' was successfully created at '$backup_directory/$backup_filename'."
 else
